a33fbd904518c5bb052fa4476d095641ebc11879
yanoo-o <87985090+yanoo-o@users.noreply.github.com>
2021-08-28 02:28:09 +0800

    Add files via upload

---
+++ lib/Content/Info.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:kaagapay/Animations/ScaleRoute.dart';
+import 'package:kaagapay/Content/Diseases.dart';
+import 'package:kaagapay/Content/Hospitals.dart';
+import 'package:kaagapay/Content/Covid.dart';
+
+class InfoPage extends StatefulWidget {
+  @override
+  _InfoPageState createState() => _InfoPageState();
+}
+class _InfoPageState extends State<InfoPage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Mga Karagdagang Impormasyon", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.indigo,
+      ),
+      body: Builder(
+          builder: (context){
+            return ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.add_location),
+                  title: Text('Malapit na Ospital at Klinik'),
+                  onTap: () => {Navigator.push(context, ScaleRoute(page: HospPage()))},
+                ),
+                ListTile(
+                  leading: Icon(Icons.notes),
+                  title: Text('Impormasyon sa mga Sakit'),
+                  onTap: () => {Navigator.push(context, ScaleRoute(page: DisPage()))},
+                ),
+                ListTile(
+                  leading: Icon(Icons.notes),
+                  title: Text('Impormasyon sa COVID-19'),
+                  onTap: () => {Navigator.push(context, ScaleRoute(page: CovidPage()))},
+                ),
+                ListTile(
+                  leading: Icon(Icons.question_answer),
+                  title: Text('Mga Karaniwang Tanong (FAQs)'),
+                  onTap: () => {Navigator.of(context).pop()},
+                ),
+              ],
+            );
+          }
+      ),
+    );
+  }
+}
